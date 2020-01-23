d97772feba9d9a4ebea91f7ae7d1e00cabfa4a1c
Maycon Carlete <maycon.carlete@hotmail.com>
2020-01-23 16:26:03 -0300

    Schedule and Notifications

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,37 @@
 import * as Yup from "yup";
+import { startOfHour, parseISO, isBefore, format } from "date-fns";
+import pt from "date-fns/locale/pt";
 import Appointment from "../models/Appointment";
 import User from "../models/User";
+import File from "../models/Files";
+import Notification from "../schemas/Notification";
 
 class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null
+      },
+      order: ["date"],
+      limit: 20,
+      offset: (page - 1) * 20,
+      attributes: ["id", "date"],
+      include: [
+        {
+          model: User,
+          as: "provider",
+          attributes: ["id", "name"],
+          include: [
+            { model: File, as: "avatar", attributes: ["id", "path", "url"] }
+          ]
+        }
+      ]
+    });
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -23,10 +52,42 @@ class AppointmentController {
         .status(401)
         .json({ error: "You canonly create appointmens with providers" });
     }
+    // verifica se a data é do passado
+    const hourStart = startOfHour(parseISO(date));
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: "Past date are not permitted" });
+    }
+    // verifica se a data está disponivel
+    const checkAvailablity = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart
+      }
+    });
+    if (checkAvailablity) {
+      return res
+        .status(400)
+        .json({ error: "Appointment date are not available" });
+    }
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
       date
+    });
+    /**
+     * Notify provider
+     *  */
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMM', ás' H:mm'h' ",
+      { locale: pt }
+    );
+    const user = await User.findByPk(req.userId);
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id
     });
     return res.json(appointment);
   }
--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,11 @@ import SessionController from "./app/controllers/SessionController";
 import FileController from "./app/controllers/FileController";
 import ProviderController from "./app/controllers/ProviderController";
 import AppointmentController from "./app/controllers/AppointmentController";
+import NotificationController from "./app/controllers/NotificationController";
 
 import authMiddleware from "./app/middlewares/auth";
 import multerConfig from "./config/multer";
+import ScheduleController from "./app/controllers/ScheduleController";
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,8 +20,11 @@ routes.post("/sessions", SessionController.store);
 // Authentication Middlewares
 routes.use(authMiddleware);
 routes.put("/users", UserController.update);
-
 routes.post("/files", upload.single("file"), FileController.store);
 routes.get("/providers", ProviderController.index);
 routes.post("/appointments", AppointmentController.store);
+routes.get("/appointments", AppointmentController.index);
+routes.get("/schedule", ScheduleController.index);
+routes.get("/notifications", NotificationController.index);
+routes.put("/notifications/:id", NotificationController.update);
 export default routes;
